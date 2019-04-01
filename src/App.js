d1f69dfa2ce65e4f531fed27a21397f2c4fe45f9
SujeethJinesh <sujeethjinesh@gmail.com>
2019-04-01 01:34:52 -0400

    Push Livesite

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import ReactGA from 'react-ga';
 
 import Button from 'react-bootstrap/Button';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    ReactGA.initialize('UA-000000-01');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+
+    console.log("Very sly of you to see the console logs. You won't find anything of value here. As you might imagine, this site is built as an April Fools joke by a Georgia Tech student to poke fun at our 'rivalry' with UGA. No harm intended. For the original Diploma image, see the following link: https://cdn.shopify.com/s/files/1/0533/3437/products/Diploma_Frame_-_University_of_Georgia_-_Graduation_Gift_-_Mahogany_Bead_-_with_Medallion_-_Black_on_Red_mat_1800x1800.jpg?v=1406567030")
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    window.print();
+  }
+  
   render() {
     return (
       <div className="App">
         <div className="diploma">
-          <input type="text" className="enterName" placeholder="Insert Name Here" onFocus="if(this.value==this.defaultValue)this.value='';"/>
+          <input id="nameInput" type="text" className="enterName" placeholder="Insert Name Here" onFocus="if(this.value==this.defaultValue)this.value='';"/>
         </div>
         <div className="printButton">
-          <Button variant="outline-dark" size="lg">
+          <Button variant="outline-dark" size="lg" onClick={this.handleClick}>
             Print your diploma
           </Button>
         </div>
